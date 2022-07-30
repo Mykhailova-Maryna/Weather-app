1a4a8c1dbaf38a88b90c986af3f04ab6a9ee39b6
Mykhailova-Maryna <mykhailovamaryna7@gmail.com>
2022-07-30 13:37:53 +0300

    Add workong forecast with new icons

--- src/script.js
+++ src/script.js
@@ -41,24 +41,26 @@ function currentDate() {
 }
 currentDay.innerHTML = currentDate();
 
-//add a search engine, when searching for a city
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", cityTemperature);
-
 //Current Weather Engine
 const apiKey = "0022112d7af3f4e205706c3973aa66eb";
-
-function cityTemperature(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-  let city = cityInput.value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
+function search(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
 }
+
+function searchManual(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  search(city.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchManual);
+
+search("Odesa");
 
 //Weather according to my GEO
 function showWeather(response) {
-  console.log(response.data);
   let city = document.querySelector("#city");
   let temperature = document.querySelector("#temp");
   let description = document.querySelector("#description");
@@ -73,7 +75,9 @@ function showWeather(response) {
   description.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
-  icon.setAttribute("src", `icons/${response.data.weather[0].icon}.png`);
+  icon.setAttribute("src", `icons/${response.data.weather[0].icon}.svg`);
+
+  getForecast(response.data.coord);
 }
 
 function showPosition(position) {
@@ -112,3 +116,44 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+//FORECAST
+function getForecast(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+
+function showForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-sm next-days">
+    <h6 class="weather-forecast-date">${formatDay(forecastDay.dt)}</h6>
+   <img src="icons/${
+     forecastDay.weather[0].icon
+   }.svg" class="forecast-icon" id="icon" width="48" />
+    <div>
+    <span class="weather-forecast-temp-max">${Math.round(
+      forecastDay.temp.max
+    )}</span>° /
+    <span class="weather-forecast-temp-min">${Math.round(
+      forecastDay.temp.min
+    )}</span>°
+    </div>
+    </div>
+  `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  return days[day];
+}
